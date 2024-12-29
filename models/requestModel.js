ec65936e143d52ca1c46f7e9bc7e8ac94af11f3c
Blithckreg <mykyta.oleshchenko@nure.ua>
2024-12-29 14:36:00 +0200

    Update request Model

--- models/requestModel.js
+++ models/requestModel.js
@@ -3,14 +3,14 @@ const pool = require('../db/connection');
 class RequestsModel {
   // Получить все запросы
   static async getAllRequests() {
-    const query = 'SELECT * FROM public."Requests"';
+    const query = 'SELECT * FROM public."requests"';
     const { rows } = await pool.query(query);
     return rows;
   }
 
   // Получить запрос по ID
   static async getRequestById(id) {
-    const query = 'SELECT * FROM public."Requests" WHERE request_id = $1';
+    const query = 'SELECT * FROM public."requests" WHERE request_id = $1';
     const { rows } = await pool.query(query, [id]);
     return rows[0];
   }
@@ -18,7 +18,7 @@ class RequestsModel {
   // Создать новый запрос
   static async createRequest({ user_id, project_name, status }) {
     const query = `
-      INSERT INTO public."Requests" (user_id, project_name, status)
+      INSERT INTO public."requests" (user_id, project_name, status)
       VALUES ($1, $2, $3)
       RETURNING *;
     `;
@@ -29,7 +29,7 @@ class RequestsModel {
   // Обновить запрос по ID
   static async updateRequest(id, { user_id, project_name, status }) {
     const query = `
-      UPDATE public."Requests"
+      UPDATE public."requests"
       SET user_id = $1, project_name = $2, status = $3
       WHERE request_id = $4
       RETURNING *;
@@ -40,7 +40,7 @@ class RequestsModel {
 
   // Удалить запрос по ID
   static async deleteRequest(id) {
-    const query = 'DELETE FROM public."Requests" WHERE request_id = $1 RETURNING *;';
+    const query = 'DELETE FROM public."requests" WHERE request_id = $1 RETURNING *;';
     const { rows } = await pool.query(query, [id]);
     return rows[0];
   }
