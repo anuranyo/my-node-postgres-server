c731dcbe0ace5712c29a2d966abeaf9d7ba49861
Blithckreg <mykyta.oleshchenko@nure.ua>
2024-12-30 15:13:43 +0200

    Update project model and controller

--- controllers/activeProjectController.js
+++ controllers/activeProjectController.js
@@ -33,7 +33,7 @@ exports.createProject = async (req, res, next) => {
   try {
     const query = `
       INSERT INTO public."activeprojects" (name, members_count, join_date, progress, deadline, description, user_id)
-      VALUES ($1, $2, $3, $4, $5, $6)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
     `;
     const { rows } = await pool.query(query, [name, members_count, join_date, progress, deadline, description, userId]);
